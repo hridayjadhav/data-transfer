f75f35a2419053f5752a92e673ba04d725ba1259
Hriday <hridayjadhav789@gmail.com>
2023-08-28 18:21:10 +0530

    using viewchild decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,17 @@ import { ChildComponent } from './child/child.component';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  @ViewChild(ChildComponent) view ! : ChildComponent
+  @ViewChild(ChildComponent) viewData ! : ChildComponent     //using viewchild() decorator
   title = 'data-transfer';
   inputName = '';
   inputName2 = '';
   inputObj = {"name":'', "surname":''};
+  response:any;
+
   TransferData(name:any, surname:any){
     this.inputName = name; 
     this.inputName2 = surname;   //here we can put 2 or more files too.
     this.inputObj = {"name":name, "surname":surname};   //here we can use both things in one.
+    this.response = this.viewData.updateList(this.inputObj);
   }
 }
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -7,11 +7,20 @@ import { Component, Input } from '@angular/core';
 })
 export class ChildComponent {
 
-  @Input() nameData : any;
+  @Input() nameData : any;   //using input decorator
   @Input() surnameData : any;
   @Input() objData : any;
 
   constructor(){}
+
+  
+
+  listArr = [{"name" : "Hriday", "surname" : "Jadhav"}]
+
+  updateList (obj:any){
+    this.listArr.push(obj);
+    return obj.name + " is added.";
+  }
 
 
 
